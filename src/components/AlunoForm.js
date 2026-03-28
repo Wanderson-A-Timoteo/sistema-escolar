6d3f6a3949cf509a410e6b195892bbbd217a2809
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2026-03-28 19:58:17 -0400

    refactor: isola CSS do AlunoForm, adiciona label e padroniza layout e ajusta cor do botao do LivroForm para manter padrao visual

--- src/components/AlunoForm.js
+++ src/components/AlunoForm.js
@@ -1,27 +1,31 @@
 import { useState } from "react";
-import styles from '../pages/CadastrarAlunos.module.css'
+import styles from './AlunoForm.module.css';
 
-function AlunoForm({adicionarAluno}){
-
+function AlunoForm({ adicionarAluno }) {
     const [nome, setNome] = useState('');
 
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault();
 
-        if(!nome.trim()) return;
+        if (!nome.trim()) return;
 
         adicionarAluno(nome);
         setNome('');
     }
 
-    return(
+    return (
         <form onSubmit={handleSubmit} className={styles.form}>
-            <input 
-                type="text"
-                placeholder="Nome do aluno"
-                value={nome}
-                onChange={(e) => setNome(e.target.value)}
-            />
+            <div className={styles.grupoInput}>
+                <label htmlFor="nomeAluno">Nome do Aluno:</label>
+                <input
+                    id="nomeAluno"
+                    type="text"
+                    placeholder="Ex: Maria da Silva"
+                    value={nome}
+                    onChange={(e) => setNome(e.target.value)}
+                />
+            </div>
+            
             <button type="submit">Adicionar</button>
         </form>
     );
